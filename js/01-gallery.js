45e597513083fddd948409e7792d94642503a362
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-13 13:17:25 +0200

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const newGallery = (items) => {
+    return (
+        galleryItems
+            .map(
+                (item) =>
+                    `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+               <img
+                  class="gallery__image"
+                  src="${item.preview}"
+                  data-source="${item.original}"
+                  alt="${item.description}"
+               />
+            </a>
+         </div>`
+            )
+            .join("")
+    );
+};
+
+gallery.insertAdjacentHTML("beforeend", newGallery(galleryItems));
+
+
+function clickOnImage(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const selectedImg = evt.target.getAttribute("data-source");
+    const instance = basicLightbox.create(`
+    <img src="${selectedImg}" width="1200" height="800">
+`)
+
+    instance.show();
+
+    gallery.addEventListener('keydown', (closed) => {
+        if (closed.key !== "ESCAPE") {
+            instance.close();
+        }
+    });
+}
+
+gallery.addEventListener("click", clickOnImage);
