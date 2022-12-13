6bc47065b7d8e2b8b88ea7742628a7474c20fcac
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-13 13:38:20 +0200

    Task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const newGallery = (items) => {
+    return (
+        galleryItems
+            .map(
+                (item) =>
+                    `<li>
+                        <a class="gallery__item" href="${item.original}">
+                            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                        </a>
+                    </li>`)
+            .join("")
+    );
+};
+
+gallery.insertAdjacentHTML("beforeend", newGallery(galleryItems));
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+});
